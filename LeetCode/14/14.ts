eeb3e6572cb94cae95b429413a8e23f53234a71d
You Hao <youhao0809@outlook.com>
2023-03-06 14:41:43 +0800

    LeetCode 14

---
+++ LeetCode/14/14.ts
@@ -0,0 +1,12 @@
+function longestCommonPrefix(strs: string[]): string {
+    let prefix: string = strs[0];
+    for (let i: number = 1; i < strs.length; i++) {
+        while (strs[i].indexOf(prefix) !== 0) {
+            prefix = prefix.substring(0, prefix.length - 1)
+            if (prefix == "") {
+                return "";
+            }
+        }
+    }
+    return prefix;
+};
